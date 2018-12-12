99911b628a31759e4b04020876d39e2f39345c87
QuenOlivier <quentin.olivier1996@gmail.com>
2018-12-12 11:38:07 +0100

    Added get_map script

--- mapping/src/get_map.py
+++ mapping/src/get_map.py
@@ -4,11 +4,11 @@ import rospy
 from PIL import Image
 import cv2
 import roslib
-import numpy
+import numpy as np
 import math
 import argparse
 from sensor_msgs.msg import Joy
-from nav_msgs.msg import MapMetaData,OccupancyGrid
+from nav_msgs.srv import GetMap
 from geometry_msgs.msg import Pose,Twist, Vector3
 
 ## \file template_class.py
@@ -20,31 +20,51 @@ from geometry_msgs.msg import Pose,Twist, Vector3
 class HectorMap:
 	
 	def __init__(self):
-		self._meta=MapMetaData()
-		self._map = numpy.zeros((1,1))
-		self._rate=10
-		self._turn=0
-		rospy.Rate(1)	
-		self._joy_sub=rospy.Subscriber("map", OccupancyGrid, self.getMap)
+		self._height=0
+		self._width=0
+		self._map=np.zeros((1,1), np.uint8)
+		self._resolution=0
+		self._origin=Pose()
 	
 	##Fetch the data.
 	#
 	# \details Extract the information from the ROS message and set it in a usable format (e.g float instead of str).
-	def getMap(self,data):
-		if(self._turn%self._rate==0):
-			self._meta=data.info
-			self._map=numpy.zeros((self._meta.width,self._meta.height))
-			for i in range(self._meta.height):
-				for j in range(self._meta.width):
-					self._map[j,i]=data.data[j+i*self._meta.width]
-		self._turn=self._turn+1
-		
+	def updateMap(self):
+		rospy.wait_for_service('/dynamic_map')
+		try:
+			getData = rospy.ServiceProxy('/dynamic_map', GetMap)
+			data = getData().map
+			self._height=data.info.height
+			self._width=data.info.width
+			self._resolution=data.info.resolution
+			self._origin=data.info.origin
+			print("Data : ")
+			print( self._width )
+			print(self._height)
+			#New test
+			print(len(data.data))
+			self._map = np.zeros((self._height,self._width),np.uint8)
+			i=self._height-1
+			j=0
+			current=0
+			while i>=0:
+				while j<=self._width-1:
+					value=data.data[current]
+					if value == -1:
+						self._map[i,j]=0
+					if value != -1:
+						self._map[i,j]=255
+					current=current+1
+					j=j+1
+				i=i-1
+				j=0
+		except rospy.ServiceException, e:
+			print "Service call failed: %s"%e
 		
 
-		###########################################
-		# Write your own code here
-		###########################################
-
+		
+   	
+	
 ## \brief This is your main function
 # \details This is where you will put your argument parseer lines and main code
 def main():
@@ -56,14 +76,16 @@ def main():
 		rosrun sawyer_robot_code <your_script_name>
 	"""
 
-	print "Initializing ROS node...\n\n"
-	rospy.init_node("Main",anonymous=True)
 	print "Doing stuff...\n"
 
 	test=HectorMap()
-
+	test.updateMap()
 	while(1):
+		cv2.namedWindow('map hector', 0)
+	    	# refresh the image on the screen
 		cv2.imshow('map hector',test._map)
+		cv2.waitKey(3)
+	
 
 
 
