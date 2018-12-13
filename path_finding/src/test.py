cfa663aa68cebc3c0f41342fa66fef90d9cbb245
QuenOlivier <quentin.olivier1996@gmail.com>
2018-12-13 10:59:00 +0100

    Correction of the visualisation of the navigation goal

--- path_finding/src/test.py
+++ path_finding/src/test.py
@@ -5,16 +5,47 @@ import numpy
 import cv2
 import rospy
 from util import map
-from geometry_msgs.msg import PoseStamped, Pose
+from geometry_msgs.msg import PoseStamped, Pose, Point
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+class PrintMap:
+	
+	def __init__(self):
+		self._map=map.Map()
+		self._flag=False
+		self._goal=Point()
+		self._sub=rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.callback)
+		
+
+	def callback(self, data):
+		self.getMap()
+    		self._goal=data.pose.position
+		self._flag=True
+		self.printData()
+		
+
+	def getMap(self):
+		self._map.updateMap()
+
+	def printData(self):
+		print("PrintMap data :\n\nFlag : ")
+		print(self._flag)
+		print("\nGoal : ")
+		print(self._goal)
+		print("\n")
+	
 
 def main():
-	test=map.Map()
-	rospy.Subscriber("chatter", PoseStamped, callback)
+	test=PrintMap() 
+	cv2.namedWindow('map hector', 0)
+	# refresh the image on the screen
+	while(1):
+		img=test._map._map
+		cv2.imshow('map hector',img)
+		cv2.circle(img,(int(test._goal.x),int(test._goal.y)),2,(0,0,255),-1)
+		cv2.waitKey(3)
+	
 
-    	# spin() simply keeps python from exiting until this node is stopped
+	# spin() simply keeps python from exiting until this node is stopped
     	rospy.spin()
 	
 
