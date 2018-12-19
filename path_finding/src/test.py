2665728925f90d65abcbce8970024f1d595dd355
QuenOlivier <quentin.olivier1996@gmail.com>
2018-12-19 09:25:39 +0100

    Map retrieving script finished

--- path_finding/src/test.py
+++ path_finding/src/test.py
@@ -18,9 +18,19 @@ class PrintMap:
 
 	def callback(self, data):
 		self.getMap()
+		#Calcul position de l'objectif
     		self._goal=data.pose.position
+		self._goal.x=self._goal.x-self._map._origin.position.x
+		self._goal.y=self._goal.y-self._map._origin.position.y
+		#Reset de l'origin
+		self._map._origin.position.x=self._map._origin.position.x/self._map._resolution
+		self._map._origin.position.y=self._map._origin.position.y/self._map._resolution
+
 		self._flag=True
+		self.printMapData()
 		self.printData()
+
+		print(self._goal.y/self._map._resolution)
 		
 
 	def getMap(self):
@@ -31,7 +41,20 @@ class PrintMap:
 		print(self._flag)
 		print("\nGoal : ")
 		print(self._goal)
-		print("\n")
+		print("\nOrigin : ")
+		print(self._map._origin.position)
+		print("\n\n")
+
+	def printMapData(self):
+		print("Map data :\n\nHeight :")
+		print(self._map._height)
+		print("\nWidth : ")
+		print(self._map._width)
+		print("\nResolution : ")
+		print(self._map._resolution)
+		print("\n\n")
+		
+		
 	
 
 def main():
@@ -39,11 +62,19 @@ def main():
 	cv2.namedWindow('map hector', 0)
 	# refresh the image on the screen
 	while(1):
-		img=test._map._map
-		cv2.imshow('map hector',img)
-		cv2.circle(img,(int(test._goal.x),int(test._goal.y)),2,(0,0,255),-1)
-		cv2.waitKey(3)
-	
+		if(test._flag):
+			img=cv2.cvtColor(test._map._map, cv2.COLOR_GRAY2RGB)
+			#Draw goal
+			x=int(test._goal.x/test._map._resolution)
+			y=int(test._map._height-test._goal.y/test._map._resolution)
+			cv2.circle(img,(x,y),5,(0,0,255),-1)
+
+			#Draw origin
+			
+			#cv2.circle(img,(x,y),5,(0,255,0),-1)
+
+			cv2.imshow('map hector',img)
+			cv2.waitKey(3)
 
 	# spin() simply keeps python from exiting until this node is stopped
     	rospy.spin()
