674a36864c5f8be5d48dd764eac830a0bf9ec607
Quentin <quentin.olivier@isfm.fr>
2023-12-18 19:20:04 +0100

    Preparing mapping utils for every package

--- map_filtering/include/map_filtering/mapping_utils.hpp
+++ map_filtering/include/map_filtering/mapping_utils.hpp
@@ -1,6 +1,23 @@
 #pragma once
 
-cv::Mat toCvMat(const nav_msgs::OccupancyGrid map);
-cv::Point toCvCoordinates(const geometry_msgs::Pose pose, const nav_msgs::MapMetaData data);
-nav_msgs::OccupancyGrid toOccupancyGrid(const cv::Mat matrix);
-geometry_msgs::Point toMapCoordinates(const cv::Point pose, const nav_msgs::MapMetaData data);
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/opencv.hpp>
+
+class MapHelper
+{
+public:
+  MapHelper(){};
+  ~MapHelper(){};
+
+  void setMapMetadata(const nav_msgs::MapMetaData data){ map_metadata_ = data; };
+
+  cv::Mat toCvMat(const nav_msgs::OccupancyGrid map);
+  cv::Point toCvCoordinates(const geometry_msgs::Pose pose);
+  nav_msgs::OccupancyGrid toOccupancyGrid(const cv::Mat matrix);
+  geometry_msgs::Point toMapCoordinates(const cv::Point pose);
+
+private:
+  nav_msgs::MapMetaData map_metadata_;
+}
---
+++ map_filtering/src/mapping_utils.cpp
@@ -0,0 +1,21 @@
+#include "map_filtering/mapping_utils.hpp"
+
+cv::Mat MapHelper::toCvMat(const nav_msgs::OccupancyGrid map)
+{
+
+}
+
+cv::Point MapHelper::toCvCoordinates(const geometry_msgs::Pose pose)
+{
+
+}
+
+nav_msgs::OccupancyGrid MapHelper::toOccupancyGrid(const cv::Mat matrix)
+{
+
+}
+
+geometry_msgs::Point MapHelper::toMapCoordinates(const cv::Point pose)
+{
+  
+}
