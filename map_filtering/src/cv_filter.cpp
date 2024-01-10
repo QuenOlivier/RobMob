b9e2453aad68104ea000a03d0506542f5bc4f789
Quentin <quentin.olivier@isfm.fr>
2024-01-10 10:50:27 +0100

    Again

--- map_filtering/src/cv_filter.cpp
+++ map_filtering/src/cv_filter.cpp
@@ -12,7 +12,7 @@ size_(size)
 
 void CvMapFilter::setPath(std::string path)
 {
-  raw_image_ = cv::imread(image_path_.c_str(), cv::IMREAD_COLOR);
+  raw_image_ = cv::imread("../map/map.pgm", cv::IMREAD_COLOR);
 
   if (raw_image_.empty())
   {
